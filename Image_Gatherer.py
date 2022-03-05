c135e776ce28c7c6e3ce4dfc4be22ecc2fd09dd7
veryyynice <tom564tom@gmail.com>
2022-03-05 23:38:51 +0100

    initial commit

--- Image_Gatherer.py
+++ Image_Gatherer.py
@@ -1,22 +1,25 @@
 from bs4 import BeautifulSoup
-import urllib2
+import urllib.request
 import requests
 import urllib
+
 website = "https://store.steampowered.com/digitalgiftcards/"
-html_page = urllib2.urlopen(website)
+html_page = urllib.request.urlopen(website)
 soup = BeautifulSoup(html_page, "html.parser")
 images = []
 links = []
+
 def get_images(website):
     for img in soup.findAll('img'):    
         images.append(img.get('src'))    
-        for link in images:
-            if link[:1] == "/" or "\\":
-                links.append(website+link)
-            else: links.append(link)
-            for i in links:
-                dawd = i[i.rfind("/") +1:] or i[i.rfind("\\") +1:]
-                dawd = dawd[:dawd.find(".")+4]
-                urllib.urlretrieve(i, dawd)  
+    for link in images:
+        if link[:1] == "/" or "\\":
+            links.append(link)
+        else: links.append(link)
+        for i in links:
+            dawd = i[i.rfind("/") +1:] or i[i.rfind("\\") +1:]
+            dawd = dawd[:dawd.find(".")+4]
+            urllib.request.urlretrieve(i, dawd)  
     print("The links:" + str(links))
+    
 get_images(website)
--- digit_recogniton.py
+++ digit_recogniton.py
@@ -1,76 +1,53 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-
-import matplotlib.pyplot as plt
-
-
 from keras.models import Sequential
-from keras.layers import Dense , Dropout , Lambda, Flatten
-from keras.optimizers import Adam ,RMSprop
-from sklearn.model_selection import train_test_split
-from keras import  backend as K
-from keras.preprocessing.image import ImageDataGenerator
-from keras.utils.np_utils import to_categorical
-from keras.layers import Convolution2D, MaxPooling2D
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+from keras.utils import np_utils
+from keras.layers.core import Dense, Activation, Dropout
 
-from subprocess import check_output
-print(check_output(["ls", "../datasets/MNIST"]).decode("utf8"))
+import pandas as pd
+import numpy as np
 
-# Any results you write to the current directory are saved as output.
+# Read data
+train = pd.read_csv('C:/Users/James/Desktop\code/datasets/MNIST/train.csv')
+labels = train.iloc[:,0].values.astype('int32')
+X_train = (train.iloc[:,1:].values).astype('float32')
+X_test = (pd.read_csv('C:/Users/James/Desktop\code/datasets/MNIST/test.csv').values).astype('float32')
 
+# convert list of labels to binary class matrix
+y_train = np_utils.to_categorical(labels) 
 
-# create the training & test sets, skipping the header row with [1:]
-train = pd.read_csv("../datasets/MNIST/train.csv")
-test = pd.read_csv("../datasets/MNIST/test.csv")
+# pre-processing: divide by max and substract mean
+scale = np.max(X_train)
+X_train /= scale
+X_test /= scale
 
-X_train = (train.iloc[:,1:].values).astype('float32') # all pixel values
-y_train = train.iloc[:,0].values.astype('int32') # only labels i.e targets digits
-X_test = test.values.astype('float32')
-X_train = X_train.reshape(X_train.shape[0], 28, 28)
+mean = np.std(X_train)
+X_train -= mean
+X_test -= mean
 
-for i in range(6, 9):
-    plt.subplot(330 + (i+1))
-    plt.imshow(X_train[i], cmap=plt.get_cmap('gray'))
-    plt.title(y_train[i]);
-X_train = X_train.reshape(X_train.shape[0], 28, 28,1)
+input_dim = X_train.shape[1]
+nb_classes = y_train.shape[1]
 
+# Here's a Deep Dumb MLP (DDMLP)
+model = Sequential()
+model.add(Dense(128, input_dim=input_dim))
+model.add(Activation('relu'))
+model.add(Dropout(0.15))
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dropout(0.15))
+model.add(Dense(nb_classes))
+model.add(Activation('softmax'))
 
-X_test = X_test.reshape(X_test.shape[0], 28, 28,1)
-mean_px = X_train.mean().astype(np.float32)
-std_px = X_train.std().astype(np.float32)
+# we'll use categorical xent for the loss, and RMSprop as the optimizer
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
 
-def standardize(x): 
-    return (x-mean_px)/std_px
+print("Training...")
+model.fit(X_train, y_train, epochs=1, batch_size=16, validation_split=0.1, verbose=2)
 
-y_train= to_categorical(y_train)
-num_classes = y_train.shape[1]
-seed = 43
-np.random.seed(seed)
+print("Generating test predictions...")
+preds = model.predict(X_test, verbose=0)
 
+#def write_preds(preds, fname):
+#    pd.DataFrame({"ImageId": list(range(1,len(preds)+1)), "Label": preds}).to_csv(fname, index=False, #header=True)
 
-
-
-
-def get_cnn_model():
-    model = Sequential([
-        Lambda(standardize, input_shape=(28,28,1)),
-        Convolution2D(32,(3,3), activation='relu'),
-        Convolution2D(32,(3,3), activation='relu'),
-        MaxPooling2D(),
-        Convolution2D(64,(3,3), activation='relu'),
-        Convolution2D(64,(3,3), activation='relu'),
-        MaxPooling2D(),
-        Flatten(),
-        Dense(512, activation='relu'),
-        Dense(10, activation='softmax')
-        ])
-    model.compile(Adam(), loss='categorical_crossentropy',
-                  metrics=['accuracy'])
-    return model
-
-model= get_cnn_model()
-model.optimizer.lr=0.01
-history=model.fit_generator(generator=batches, steps_per_epoch=batches.n, epochs=1, 
-                    validation_data=val_batches, validation_steps=val_batches.n)
+#write_preds(preds, "keras-mlp.csv")
+print(preds)
